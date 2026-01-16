6736fac8fdf54d396b2752fa7cabfcdfff05ae60
Chad Cox <32845448+chadmcox@users.noreply.github.com>
2026-01-16 13:11:12 -0700

    Add documentation reference for health issues
    Added a comment with a reference link to the Microsoft Graph documentation for security health issues.

--- PowerShell/Get-MDIOpenHealthIssues.ps1
+++ PowerShell/Get-MDIOpenHealthIssues.ps1
@@ -1,5 +1,7 @@
 connect-mggraph SecurityIdentitiesHealth.Read.All
 
+# graph reference: https://learn.microsoft.com/en-us/graph/api/resources/security-healthissue?view=graph-rest-beta
+
 get-MgBetaSecurityIdentityHealthIssue -filter "Status eq 'open'" | foreach{$hi = $_
     $hi | select Id -ExpandProperty AdditionalInformation | convertto-json -Depth 99 | convertfrom-json | foreach{
       $_ | select  @{N="Id";E={$hi.ID}}, `
