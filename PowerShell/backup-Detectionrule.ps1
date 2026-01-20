21b22039815feb5a6a9c0d77f46dcc415efb1f01
Chad Cox <32845448+chadmcox@users.noreply.github.com>
2026-01-20 09:45:59 -0700

    Create backup-Detectionrule.ps1

---
+++ PowerShell/backup-Detectionrule.ps1
@@ -0,0 +1,6 @@
+#https://learn.microsoft.com/en-us/graph/api/security-detectionrule-list?view=graph-rest-beta&tabs=http
+connect-mggraph -scope CustomDetection.Read.All
+
+Get-MgBetaSecurityRuleDetectionRule -all | foreach{
+    $_ | convertto-json -Depth 99 | out-file -FilePath "c:\data\Detectionbackup\$($_.DisplayName).json"
+}
