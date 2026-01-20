c8da9235c0f9523be1b9a29b2b7576a5ae25218c
Chad Cox <32845448+chadmcox@users.noreply.github.com>
2026-01-20 09:46:51 -0700

    Update backup-Detectionrule.ps1

--- PowerShell/backup-Detectionrule.ps1
+++ PowerShell/backup-Detectionrule.ps1
@@ -1,3 +1,5 @@
+param($defaultpath="$env:USERPROFILE\downloads")
+
 #https://learn.microsoft.com/en-us/graph/api/security-detectionrule-list?view=graph-rest-beta&tabs=http
 connect-mggraph -scope CustomDetection.Read.All
 
