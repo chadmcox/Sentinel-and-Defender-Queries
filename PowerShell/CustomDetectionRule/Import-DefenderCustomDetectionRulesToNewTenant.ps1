5afdce59d1f76bb79874b6721fa8867176164c94
Chad Cox <32845448+chadmcox@users.noreply.github.com>
2026-01-26 10:55:24 -0700

    Update Import-DefenderCustomDetectionRulesToNewTenant.ps1
    Added requirements for specific PowerShell module versions and connection commands.

--- PowerShell/CustomDetectionRule/Import-DefenderCustomDetectionRulesToNewTenant.ps1
+++ PowerShell/CustomDetectionRule/Import-DefenderCustomDetectionRulesToNewTenant.ps1
@@ -1,5 +1,12 @@
+#Requires -Modules Microsoft.Graph.Beta.Security, Microsoft.Graph.Authentication
 param($path="$env:USERPROFILE\downloads")
 
+#the cmdlets work with version 2.24 powershell
+#to install that version run
+#find-module Microsoft.Graph.Beta.Security -RequiredVersion 2.24.0 | install-module
+#find-module Microsoft.Graph.Authentication -RequiredVersion 2.24.0 | install-module
+
+#or you can run it in newer powershell with latest release of modules
 Disconnect-MgGraph
 connect-mggraph -Scopes CustomDetection.Read.All,CustomDetection.ReadWrite.All
 
